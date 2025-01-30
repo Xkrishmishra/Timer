02c2398fcc2c7a1d814ea3ee54f36d33bae1c378
𝐊ʀɪsʜ 𝐌ɪsʜʀᴀ <163736172+Xkrishmishra@users.noreply.github.com>
2025-01-30 14:55:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -102,4 +102,16 @@ function exitFullScreen() {
     }
 }
 
+// Register Service Worker for Offline Support
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("sw.js")
+        .then(() => console.log("Service Worker Registered"))
+        .catch((error) => console.log("Service Worker Registration Failed:", error));
+}
+
+updateDisplay();
+tFullscreen();
+    }
+}
+
 updateDisplay();
