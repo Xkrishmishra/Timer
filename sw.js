e5becb0f13bef931c4d50adb3ce38501e2a7ecc6
𝐊ʀɪsʜ 𝐌ɪsʜʀᴀ <163736172+Xkrishmishra@users.noreply.github.com>
2025-01-30 14:56:34 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "countdown-timer-cache-v1";
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./sweet.mp3",
+];
+
+// Install the Service Worker and cache all files
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
+
+// Serve files from the cache
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Update cache if files change
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(
+                keyList.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
